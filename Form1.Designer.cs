16b51c704dc86c6899b3b822d928abe04722a710
Jhonny <juanchitorodi1312@gmail.com>
2023-02-12 17:42:22 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace PerceptronSimple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tbxPeso1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxPeso2 = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.tbxBias = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart2
+            // 
+            this.chart2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisX2.TitleFont = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY2.TitleFont = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(324, 12);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            this.chart2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.LabelBorderWidth = 2;
+            series1.LabelForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 10;
+            series1.Name = "Validos";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series2.LabelBackColor = System.Drawing.Color.Red;
+            series2.LabelBorderWidth = 2;
+            series2.LabelForeColor = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.MarkerSize = 10;
+            series2.Name = "No validos";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Linea";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Legend = "Legend1";
+            series4.MarkerSize = 10;
+            series4.Name = "Sin asignar";
+            this.chart2.Series.Add(series1);
+            this.chart2.Series.Add(series2);
+            this.chart2.Series.Add(series3);
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(539, 521);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart1";
+            this.chart2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.chart2_MouseClick);
+            // 
+            // tbxPeso1
+            // 
+            this.tbxPeso1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbxPeso1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxPeso1.Location = new System.Drawing.Point(75, 166);
+            this.tbxPeso1.Name = "tbxPeso1";
+            this.tbxPeso1.Size = new System.Drawing.Size(154, 35);
+            this.tbxPeso1.TabIndex = 2;
+            this.tbxPeso1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(116, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Peso 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(116, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Peso 2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tbxPeso2
+            // 
+            this.tbxPeso2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbxPeso2.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxPeso2.Location = new System.Drawing.Point(75, 252);
+            this.tbxPeso2.Name = "tbxPeso2";
+            this.tbxPeso2.Size = new System.Drawing.Size(154, 35);
+            this.tbxPeso2.TabIndex = 6;
+            this.tbxPeso2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(89)))), ((int)(((byte)(81)))));
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresar.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.ForeColor = System.Drawing.Color.White;
+            this.btnIngresar.Location = new System.Drawing.Point(37, 413);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(240, 40);
+            this.btnIngresar.TabIndex = 11;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = false;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // tbxBias
+            // 
+            this.tbxBias.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbxBias.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxBias.Location = new System.Drawing.Point(75, 345);
+            this.tbxBias.Name = "tbxBias";
+            this.tbxBias.Size = new System.Drawing.Size(154, 35);
+            this.tbxBias.TabIndex = 13;
+            this.tbxBias.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(126, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 30);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Bias";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(65)))), ((int)(((byte)(89)))));
+            this.ClientSize = new System.Drawing.Size(875, 545);
+            this.Controls.Add(this.tbxBias);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.tbxPeso2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxPeso1);
+            this.Controls.Add(this.chart2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Perceptron Simple";
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TextBox tbxPeso1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxPeso2;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox tbxBias;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PerceptronSimple
+{
+    public partial class Form1 : Form
+    {
+        private List<double> valorX = new List<double>();
+        private List<double> valorY = new List<double>();
+
+        private double w0 = 1;
+        private double w1 = 1;
+        private double w2 = 1;
+        private double b = -1.5;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Maximos, minimos y linea cruzada
+            chart2.ChartAreas[0].AxisX.Minimum = -10;
+            chart2.ChartAreas[0].AxisY.Minimum = -10;
+            chart2.ChartAreas[0].AxisX.Maximum = 10;
+            chart2.ChartAreas[0].AxisY.Maximum = 10;
+            chart2.ChartAreas[0].AxisX.Crossing = 0;
+            chart2.ChartAreas[0].AxisY.Crossing = 0;
+
+            //Configuraciones extras de diseño
+            chart2.ChartAreas[0].AxisX.ArrowStyle = AxisArrowStyle.Triangle;
+            chart2.ChartAreas[0].AxisY.ArrowStyle = AxisArrowStyle.Triangle;
+            chart2.ChartAreas[0].AxisX.LineWidth= 2;
+            chart2.ChartAreas[0].AxisY.LineWidth= 2;
+            chart2.ChartAreas[0].AxisX.LabelAutoFitMinFontSize = 14;
+            chart2.ChartAreas[0].AxisY.LabelAutoFitMinFontSize = 14;
+
+            chart2.ChartAreas[0].BackColor = Color.Transparent;
+
+            //Colores
+            chart2.Series[0].Color = Color.Green;
+            chart2.Series[1].Color = Color.Red;
+            chart2.Series[2].Color = Color.Black;
+            chart2.Series[3].Color = Color.Black;
+        }
+
+        private void chart2_MouseClick(object sender, MouseEventArgs e)
+        {
+            valorX.Add(chart2.ChartAreas[0].AxisX.PixelPositionToValue(e.X));
+            valorY.Add(chart2.ChartAreas[0].AxisY.PixelPositionToValue(e.Y));
+
+            chart2.Series[3].Points.AddXY(valorX.Last(), valorY.Last());
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(tbxPeso1.Text) || String.IsNullOrEmpty(tbxPeso2.Text) || String.IsNullOrEmpty(tbxBias.Text))
+            {
+                MessageBox.Show("Llene el formulario correctamente");
+                return;
+            }
+            if (chart2.Series[0].Points.Count < 5 &&
+                chart2.Series[1].Points.Count < 5 && 
+                chart2.Series[3].Points.Count < 5)
+            {
+                MessageBox.Show("Ingrese almenos 5 puntos en el plano");
+                return;
+            }
+
+            chart2.Series[0].Points.Clear();
+            chart2.Series[1].Points.Clear();
+            chart2.Series[2].Points.Clear();
+            chart2.Series[3].Points.Clear();
+
+            w1 = double.Parse(tbxPeso1.Text);
+            w2 = double.Parse(tbxPeso2.Text);
+            b = double.Parse(tbxBias.Text);
+
+            LimpiarTextos();
+
+
+            for (int i = 0; i < valorX.Count; i++)
+            {
+                if (ValidarEstado(valorX[i], valorY[i]))
+                {
+                    chart2.Series[0].Points.AddXY(valorX[i], valorY[i]);
+                }
+                else
+                {
+                    chart2.Series[1].Points.AddXY(valorX[i], valorY[i]);
+                }
+            }
+
+            var y1 = (-b - w1 * (valorX.Min()) / w2);
+            var y2 = (-b - w1 * (valorX.Max()) / w2);
+
+            chart2.Series[2].Points.Clear();
+            chart2.Series[2].Points.AddXY(valorX.Min(), y1);
+            chart2.Series[2].Points.AddXY(valorX.Max(), y2);
+        }
+
+
+        #region Validaciones
+
+        private bool ValidarEstado(double x, double y)
+        {
+            return (w0 * b) + (x * w1) + (y * w2) >= 0;
+        }
+
+        private void LimpiarTextos()
+        {
+            tbxPeso1.Clear();
+            tbxPeso2.Clear();
+            tbxBias.Clear();
+        }
+
+        #endregion
+    }
+}
